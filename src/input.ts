805fcd809a6d5d3be97b4de9e543b8da9b7c06ea
chiba <4kgtnk2575@gmail.com>
2025-07-13 21:14:20 +0900

    fix keydown input

--- src/input.ts
+++ src/input.ts
@@ -24,6 +24,7 @@ export class Input {
   ) {
     // TODO: be careful enough for these eventlistener
     document.addEventListener('keydown', e => {
+      if (!stateHandle.duringGamePlayWithoutJustDrawing()) return;
       e.preventDefault();
       // is this condition necessary? yes for rotation
       // if (this.canTakeInput()) {
@@ -47,6 +48,8 @@ export class Input {
 
     // for quickturn
     document.addEventListener('keydown', keyPressedTwice('x', () => {
+      if (!stateHandle.duringGamePlayWithoutJustDrawing()) return;
+
       if (this._rotate.quickTurn.isPossible && this._current.currentPuyo) {
         this._rotate.quickTurn.willExecute = true;
         // quickTurn.turnCW();
@@ -56,6 +59,8 @@ export class Input {
     }, () => { this._rotate.quickTurn.willExecute = false; }, 1000));
 
     document.addEventListener('keydown', keyPressedTwice('z', () => {
+      if (!stateHandle.duringGamePlayWithoutJustDrawing()) return;
+
       if (this._rotate.quickTurn.isPossible && this._current.currentPuyo) {
         this._rotate.quickTurn.willExecute = true;
         // quickTurn.turnACW();
@@ -65,6 +70,8 @@ export class Input {
     }, () => { this._rotate.quickTurn.willExecute = false; }, 1000));
 
     document.addEventListener('keyup', e => {
+      // if (!stateHandle.duringGamePlayWithoutJustDrawing()) return;
+      //
       // if (canTakeInput()) {
       if (e.key === 'ArrowLeft') {
         this.isLeftKeyPressed = false;
